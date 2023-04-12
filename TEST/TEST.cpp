86ccc742d95ee31901feb49cd0c276ee9d737c76
trakt <trakt@DESKTOP-UDGVRCB>
2023-04-12 19:35:02 +0300

    Добавьте файлы проекта.

---
+++ TEST/TEST.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main() {
+    system("color 0C");
+    cout << "I love C++!\n\tI love C++!\n\t\tI love C++!\n\t\t\tI love C++!\n\t\t\t\tI love C++!";
+}
